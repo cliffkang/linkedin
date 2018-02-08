360f0f588e1059fce10581365242ee0f72073fd4
cliffkang <cliffgkang@gmail.com>
2018-02-07 22:31:53 -0800

    added a few more js files for structure purposes

---
+++ src/components/RightColumn/PromotedContent/PromotedContent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function PromotedContent() {
+  return(
+    <div>
+      <img alt='icon' />
+      <div>
+        <h4>{}</h4>
+        <p>{}</p>
+      </div>
+      <img alt='right arrow' />
+    </div>
+  )
+}
+
+export default PromotedContent;
