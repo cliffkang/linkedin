3b19a1dc4a5736e493cdc2006fb6cf53510d9bcb
cliffkang <cliffgkang@gmail.com>
2018-02-07 21:05:21 -0800

    set up file directory for all components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import Heading from './components/Heading/Heading.js';
+import Ad from './components/Ad/Ad.js';
+import LeftColumn from './components/LeftColumn/LeftColumn.js';
+import CenterColumn from './components/CenterColumn/CenterColumn.js';
+import RightColumn from './components/RightColumn/RightColumn.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Heading />
+        <Ad />
+        <LeftColumn />
+        <CenterColumn />
+        <RightColumn />
       </div>
     );
   }
